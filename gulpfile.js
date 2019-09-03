bac6f0b7bdcb7778683c5da5bd4477cb1bf85a6e
Beate <bema@bespiel.com>
2019-09-03 05:15:41 +0200

    Concat added

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,31 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
-    coffee = require('gulp-coffee');
+    coffee = require('gulp-coffee'),
+    concat = require('gulp-concat');
 
 /* gulp.task ('log', function() {
     gutil.log('Das macht Spaß');
 }); */
-coffeeScripts = ['components/coffee/tagline.coffee'];
+
+//javascript dateien punkt 3
+var coffeeScripts = ['components/coffee/tagline.coffee'];
+var jsSources = [
+    'components/scripts/rclick.js',
+    'components/scripts/pixgrid.js',
+    'components/scripts/tagline.js',
+    'components/scripts/template.js'
+
+];
 
 gulp.task('coffee', function(){
     gulp.src(coffeeScripts)
     .pipe(coffee({bare: true})
     .on('error', gutil.log))
     .pipe(gulp.dest('components/scripts'))
+});
+// javascript dateien in script js zusammenschreiben
+gulp.task('js', function(){
+    gulp.src(jsSources)
+    .pipe(concat('script.js'))
+    .pipe(gulp.dest('builds/development/scripts'))
 });
